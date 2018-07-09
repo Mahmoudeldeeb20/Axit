8eaad1e81595e500dd696fe1de83c26fa641274f
Mahmoudeldeeb20 <mahmoud-el-deeb@hotmail.com>
2018-07-09 17:46:09 +0200

    Update 001

--- Js/plugin.js
+++ Js/plugin.js
@@ -1,86 +1,85 @@
 $(document).ready(function() {
-    $(".tabs .tabs-icon div").on("click", function() {
-        $(this).addClass("tab-active").siblings().removeClass("tab-active")
-    });
 
-
-    $(".pricing-option .options:nth-of-type(1)").on("mouseenter", function() {
-        $(".pricing-option .options:nth-of-type(1) li:nth-child(4)").animate({
-            height: '35px'
-        }, 500);
-    });
-
-    $(".pricing-option .options:nth-of-type(1)").on("mouseleave", function() {
-        $(".pricing-option .options:nth-of-type(1) li:nth-child(4)").animate({
-            height: '5px'
-        }, 500);
-    });
-
-
-    $(".pricing-option .options:nth-of-type(2)").on("mouseenter", function() {
-        $(".pricing-option .options:nth-of-type(2) li:nth-child(4)").animate({
-            height: '35px'
-        }, 500);
-    });
-
-    $(".pricing-option .options:nth-of-type(2)").on("mouseleave", function() {
-        $(".pricing-option .options:nth-of-type(2) li:nth-child(4)").animate({
-            height: '5px'
-        }, 500);
-    });
-
+    // Change HTML Scroll Style
     $(function() {
         $("html").niceScroll({
             horizrailenabled: false,
         });
     });
-
-
-
-    $(".pricing-option .options:nth-of-type(3)").on("mouseenter", function() {
-        $(".pricing-option .options:nth-of-type(3) li:nth-child(4)").animate({
-            height: '35px'
-        }, 500);
-    });
-
-    $(".pricing-option .options:nth-of-type(3)").on("mouseleave", function() {
-        $(".pricing-option .options:nth-of-type(3) li:nth-child(4)").animate({
-            height: '5px'
-        }, 500);
-    });
-
-
-
+    // Change Nav-Bar Style When Scroll
     $(window).on("scroll", function() {
         var scroll = $(window).scrollTop();
         if (scroll > 100) {
             $(".nav").css("background", "rgba(33,33,33,0.9)")
             $(".nav").css("position", "fixed")
-            navItem.css("color", "white")
-                //  console.log($(window).scrollTop());
+
         } else {
             $(".nav").css("background", "transparent")
-                //$(".nav").css("position", "absolute")
-            navItem.css("color", "white")
-        }
 
+        }
+    });
+
+
+
+    // Tap
+    $(".tabs .tabs-icon div").on("click", function() {
+        $(this).addClass("tab-active").siblings().removeClass("tab-active")
     });
 
     $("#tab-one").on("click", function() {
         $("#tab-one-content").show().siblings().hide();
     });
 
-
-
     $("#tab-two").on("click", function() {
         $("#tab-two-content").show().siblings().hide();
     });
 
-
-
-
     $("#tab-three").on("click", function() {
         $("#tab-three-content").show().siblings().hide();
+    });
+
+    // Pricing Option
+
+    var optionOne = $(".pricing-option .options:nth-of-type(1)");
+    var optionOneLi = $(".pricing-option .options:nth-of-type(1) li:nth-child(4)");
+
+    var optionTwo = $(".pricing-option .options:nth-of-type(2)");
+    var optionTwoLi = $(".pricing-option .options:nth-of-type(2) li:nth-child(4)");
+
+    var optionThree = $(".pricing-option .options:nth-of-type(3)");
+    var optionThreeLi = $(".pricing-option .options:nth-of-type(3) li:nth-child(4)");
+
+    optionOne.on("mouseenter", function() {
+        optionOneLi.animate({
+            height: '35px'
+        }, 500);
+    });
+    optionOne.on("mouseleave", function() {
+        optionOneLi.animate({
+            height: '5px'
+        }, 500);
+    });
+
+    optionTwo.on("mouseenter", function() {
+        optionTwoLi.animate({
+            height: '35px'
+        }, 500);
+    });
+    optionTwo.on("mouseleave", function() {
+        optionTwoLi.animate({
+            height: '5px'
+        }, 500);
+    });
+
+    optionThree.on("mouseenter", function() {
+        optionThreeLi.animate({
+            height: '35px'
+        }, 500);
+    });
+    optionThree.on("mouseleave", function() {
+        optionThreeLi.animate({
+            height: '5px'
+        }, 500);
     });
 
 
